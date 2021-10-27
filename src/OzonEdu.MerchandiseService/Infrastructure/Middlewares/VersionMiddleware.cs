b3581d969ebcb7f410a5f7768cc5edb093742dc0
Vladislav Grebenyuk <v.grebenyuk@pushapp.ru>
2021-10-28 00:09:02 +0300

    [homework-2][add] Middlewares

---
+++ src/OzonEdu.MerchandiseService/Infrastructure/Middlewares/VersionMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace OzonEdu.MerchandiseService.Infrastructure.Middlewares
+{
+    public class VersionMiddleware
+    {
+        public VersionMiddleware(RequestDelegate next)
+        {
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "no version";
+            var serviceName = Assembly.GetExecutingAssembly().GetName().Name ?? "no name";
+
+            await context.Response.WriteAsync(JsonSerializer.Serialize(new { version, serviceName }));
+        }
+    }
+}
