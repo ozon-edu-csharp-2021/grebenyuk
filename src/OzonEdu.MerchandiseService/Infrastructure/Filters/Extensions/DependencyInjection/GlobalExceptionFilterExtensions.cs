1bd63f07ec532300d2aa71b0a58ca33c5d8a2014
Vladislav Grebenyuk <v.grebenyuk@pushapp.ru>
2021-10-28 00:08:34 +0300

    [homework-2][add] GlobalExceptionFilter

---
+++ src/OzonEdu.MerchandiseService/Infrastructure/Filters/Extensions/DependencyInjection/GlobalExceptionFilterExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace OzonEdu.MerchandiseService.Infrastructure.Filters.Extensions.DependencyInjection
+{
+    public static class GlobalExceptionFilterExtensions
+    {
+        public static IServiceCollection AddGlobalExceptionFilter(this IServiceCollection services)
+        {
+            services.AddControllers(options => options.Filters.Add<GlobalExceptionFilter>());
+            
+            return services;
+        }
+    }
+}
