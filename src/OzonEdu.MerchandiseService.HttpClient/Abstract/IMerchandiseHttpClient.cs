ff875d03270aae1295bd3ce687102764e8b058d9
Vladislav Grebenyuk <v.grebenyuk@pushapp.ru>
2021-10-28 16:26:47 +0300

    [homework-2][add] HttpClient

---
+++ src/OzonEdu.MerchandiseService.HttpClient/Abstract/IMerchandiseHttpClient.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using OzonEdu.MerchandiseService.HttpModels;
+
+namespace OzonEdu.MerchandiseService.HttpClient.Abstract
+{
+    public interface IMerchandiseHttpClient
+    {
+        Task<MerchandiseTicketResponse> GetByIdAsync(long id, CancellationToken token);
+        Task<MerchandiseTicketResponse> AddAsync(MerchandiseTicketPostRequest ticket, CancellationToken token);
+    }
+}
---
+++ src/OzonEdu.MerchandiseService.HttpClient/MerchandiseHttpClient.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using OzonEdu.MerchandiseService.HttpClient.Abstract;
+using OzonEdu.MerchandiseService.HttpModels;
+
+namespace OzonEdu.MerchandiseService.HttpClient
+{
+    public class MerchandiseHttpClient : IMerchandiseHttpClient
+    {
+        private readonly System.Net.Http.HttpClient _httpClient;
+
+        private const string BaseRoute = "v1/api/merchandise";
+
+        public MerchandiseHttpClient(System.Net.Http.HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        
+        public async Task<MerchandiseTicketResponse> GetByIdAsync(long employeeId, CancellationToken token)
+        {
+            using var response = await _httpClient.GetAsync($"{BaseRoute}/{employeeId}", token);
+            var body = await response.Content.ReadAsStringAsync(token);
+            return JsonSerializer.Deserialize<MerchandiseTicketResponse>(body);
+        }
+        
+        public async Task<MerchandiseTicketResponse> AddAsync(MerchandiseTicketPostRequest request, 
+            CancellationToken token)
+        {
+            using var response = await _httpClient.PostAsync($"{BaseRoute}", new StringContent(string.Empty), token);
+            var body = await response.Content.ReadAsStringAsync(token);
+            return JsonSerializer.Deserialize<MerchandiseTicketResponse>(body);
+        }
+    }
+}
