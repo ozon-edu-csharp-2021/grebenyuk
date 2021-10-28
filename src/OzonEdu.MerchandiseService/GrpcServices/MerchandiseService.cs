cfc29c06d3424d449aa37529f7f945b5cddc72e3
Vladislav Grebenyuk <v.grebenyuk@pushapp.ru>
2021-10-28 15:57:53 +0300

    [homework-2][add] Implement Grpc MerchandiseService

---
+++ src/OzonEdu.MerchandiseService/GrpcServices/MerchandiseService.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+using OzonEdu.MerchandiseService.Grpc;
+using OzonEdu.MerchandiseService.Models;
+using OzonEdu.MerchandiseService.Services.Abstract;
+
+namespace OzonEdu.MerchandiseService.GrpcServices
+{
+    public class MerchandiseService : OzonEdu.MerchandiseService.Grpc.MerchandiseService.MerchandiseServiceBase
+    {
+        private readonly IMerchandiseTicketsService _merchandiseTicketsService;
+
+        public MerchandiseService(IMerchandiseTicketsService merchandiseTicketsService)
+        {
+            _merchandiseTicketsService = merchandiseTicketsService;
+        }
+        
+        public override async Task<GetTicketByEmployeeIdReply> GetTicketByEmployeeId(
+            GetTicketByEmployeeIdRequest request, 
+            ServerCallContext context)
+        {
+            var ticket = await _merchandiseTicketsService.GetByIdAsync(request.EmployeeId, context.CancellationToken);
+            
+            return new GetTicketByEmployeeIdReply
+            {
+                Ticket = new Ticket
+                {
+                    Id = ticket.Id,
+                    EmployeeId = ticket.EmployeeId,
+                    ItemId = ticket.ItemId
+                }
+            };
+        }
+        
+        public override async Task<AddTicketReply> AddTicket(AddTicketRequest request, ServerCallContext context)
+        {
+            var ticket = await _merchandiseTicketsService.AddAsync(
+                new MerchandiseTicketCreationModel(request.EmployeeId, request.ItemId), 
+                context.CancellationToken);
+
+            return new AddTicketReply
+            {
+                Ticket = new Ticket
+                {
+                    Id = ticket.Id,
+                    EmployeeId = ticket.EmployeeId,
+                    ItemId = ticket.ItemId
+                }
+            };
+        }
+    }
+}
