702b885ff2a36ee7b9e12bdc22c2cb31d88042ba
Vladislav Grebenyuk <v.grebenyuk@pushapp.ru>
2021-10-28 15:31:51 +0300

    [homework-2][add] DI

--- src/OzonEdu.MerchandiseService/Startup.cs
+++ src/OzonEdu.MerchandiseService/Startup.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using OzonEdu.MerchandiseService.Services;
+using OzonEdu.MerchandiseService.Services.Abstract;
 
 namespace OzonEdu.MerchandiseService
 {
@@ -10,6 +12,7 @@ namespace OzonEdu.MerchandiseService
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IMerchandiseTicketsService, MerchandiseTicketsService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -19,6 +22,7 @@ namespace OzonEdu.MerchandiseService
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
             });
         }
     }
