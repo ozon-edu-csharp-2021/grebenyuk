0c65061848e254defaf2c2043ef59af7b7d0f876
Vladislav Grebenyuk <vl4disl4v.grebenyuk@gmail.com>
2021-11-11 01:47:48 +0300

    [homework-3][add] ITicketRepository

---
+++ src/OzonEdu.MerchandiseService.Domain/AggregationModels/CreateTicketAggregate/Abstract/ITicketRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using OzonEdu.MerchandiseService.Domain.AggregationModels.EmployeeAggregate;
+using OzonEdu.MerchandiseService.Domain.Contracts;
+
+namespace OzonEdu.MerchandiseService.Domain.AggregationModels.CreateTicketAggregate.Abstract
+{
+    public interface ITicketRepository : IRepository<Ticket>
+    {
+        Task<IEnumerable<Ticket>> GetAllTicketsByEmployeeIdAsync(EmployeeId employeeId, CancellationToken cancellationToken = default);
+    }
+}
