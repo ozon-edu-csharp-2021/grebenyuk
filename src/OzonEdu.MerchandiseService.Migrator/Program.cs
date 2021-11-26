62a4496514aa6f0be38c330d645d5528ca15ae6e
Vladislav Grebenyuk <vl4disl4v.grebenyuk@gmail.com>
2021-11-27 01:55:56 +0300

    [homework-4][add] Миграции

---
+++ src/OzonEdu.MerchandiseService.Migrator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OzonEdu.MerchandiseService.Migrator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            var connectionString = configuration.GetSection("DatabaseConnectionOptions:ConnectionString").Get<string>();
+
+            var services = new ServiceCollection()
+                .AddFluentMigratorCore()
+                .ConfigureRunner(c =>
+                    c.AddPostgres()
+                        .WithGlobalConnectionString(connectionString)
+                        .ScanIn(typeof(Program).Assembly)
+                        .For.Migrations()
+                );
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            using (serviceProvider.CreateScope())
+            {
+                var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+                runner.MigrateUp();
+            }
+        }
+    }
+}
