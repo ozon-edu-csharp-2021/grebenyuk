7f861a3bea6092dc62790e44280e324c40e0a49c
Vladislav Grebenyuk <vl4disl4v.grebenyuk@gmail.com>
2021-11-27 02:30:02 +0300

    [homework-4][add] DI

--- src/OzonEdu.MerchandiseService.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ src/OzonEdu.MerchandiseService.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,8 +1,14 @@
 ﻿using MediatR;
+using Microsoft.Extensions.Configuration;
 using OzonEdu.MerchandiseService.Infrastructure.Commands;
 using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
 using OzonEdu.MerchandiseService.Domain.AggregationModels.CreateTicketAggregate.Abstract;
+using OzonEdu.MerchandiseService.Domain.Contracts;
+using OzonEdu.MerchandiseService.Infrastructure.Configuration;
 using OzonEdu.MerchandiseService.Infrastructure.Repositories;
+using OzonEdu.MerchandiseService.Infrastructure.Repositories.Infrastructure;
+using OzonEdu.MerchandiseService.Infrastructure.Repositories.Infrastructure.Abstract;
 
 namespace OzonEdu.MerchandiseService.Infrastructure.Extensions
 {
@@ -15,9 +21,20 @@ namespace OzonEdu.MerchandiseService.Infrastructure.Extensions
             return services;
         }
         
+        public static IServiceCollection AddDatabaseComponents(this IServiceCollection services)
+        {
+            services.ConfigureOptions<DatabaseConnectionOptions>();
+            //services.Configure<DatabaseConnectionOptions>(configuration.GetSection(nameof(DatabaseConnectionOptions)));
+            services.AddScoped<IDbConnectionFactory<NpgsqlConnection>, NpgsqlConnectionFactory>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IChangeTracker, ChangeTracker>();
+            return services;
+        }
+
         public static IServiceCollection AddInfrastructureRepositories(this IServiceCollection services)
         {
-            services.AddSingleton<ITicketRepository, TicketRepository>();
+            Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
+            services.AddScoped<ITicketRepository, TicketRepository>();
             
             return services;
         }
